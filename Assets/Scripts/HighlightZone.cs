9218eab180240d6398e6ecca3be33b86b09c297a
AntohiRazvan <antohirazvan@gmail.com>
2021-10-03 15:23:57 +0300

    Zone highlighter

--- Assets/Scripts/HighlightZone.cs
+++ Assets/Scripts/HighlightZone.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class HighlightZone : MonoBehaviour
 {
-    public float speed = 1.0f;
+    public float speed = 8.0f;
+    Color originalColor;
     Color startColor;
     Color endColor;
     float startTime;
@@ -14,20 +15,24 @@ public class HighlightZone : MonoBehaviour
         if(animating){
             float t = (Time.time - startTime) * speed;
             gameObject.GetComponent<UnityEngine.U2D.SpriteShapeRenderer>().materials[1].color = Color.Lerp(startColor, endColor, t);
+            if(gameObject.GetComponent<UnityEngine.U2D.SpriteShapeRenderer>().materials[1].color == endColor){
+                animating = false;
+            }
         }
     }
 
     void OnMouseEnter() {
-        Debug.Log("Mouse entered");
         startTime = Time.time;
-        startColor = gameObject.GetComponent<UnityEngine.U2D.SpriteShapeRenderer>().materials[1].color;
-        endColor = new Color(startColor.r+0.5f, startColor.g+0.5f, startColor.b+0.5f, startColor.a+0.1f);
+        originalColor = gameObject.GetComponent<UnityEngine.U2D.SpriteShapeRenderer>().materials[1].color;
+        startColor = originalColor;
+        endColor = new Color(startColor.r, startColor.g, startColor.b, startColor.a+0.3f);
         animating = true;
     }
 
     void OnMouseExit() {
-        Debug.Log("Mouse exited");
         gameObject.GetComponent<UnityEngine.U2D.SpriteShapeRenderer>().materials[1].color = startColor;
-        animating = false;
+        startColor = endColor;
+        endColor = originalColor;
+        animating = true;
     }
 }
