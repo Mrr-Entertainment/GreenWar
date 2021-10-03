6d7d6fa3393fb366e3e81a2a59351687308350e2
Razvan Rotari <razvanrotari@posteo.net>
2021-10-03 13:19:50 +0300

    Improved the GUI

--- Assets/Scripts/TreeManager.cs
+++ Assets/Scripts/TreeManager.cs
@@ -1,31 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TreeManager : MonoBehaviour
 {
 	public Region[] regions;
 	public int income = 0;
-    void Start()
-    {
-         regions = FindObjectsOfType(typeof(Region)) as Region[];
-    }
+	public TextMeshProUGUI incomeText;
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
+	void Start()
+	{
+		regions = FindObjectsOfType(typeof(Region)) as Region[];
+		incomeText.text = income + "$";
+	}
+
+	void Update()
+	{
 		foreach(Region region in regions) {
 			if (Time.time - region.lastIncomeTime < 2f) {
 				continue;
 			}
 			region.lastIncomeTime = Time.time;
-			// Only player 
+			// Only player
 			if (region.enemyUnits.Count == 0 && region.playerUnits.Count != 0) {
 				int sum = 0;
 				foreach (var unit in region.playerUnits) {
 					sum+= unit.incomePower;
 				}
 				income += sum;
+				incomeText.text = income + "$";
 				region.treeCount -= sum;
 				//Show popup with money income
 
@@ -38,5 +42,5 @@ public class TreeManager : MonoBehaviour
 				region.treeCount += sum;
 			}
 		}
-    }
+	}
 }
